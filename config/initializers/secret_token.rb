4cf2b0821afc9d223b1b019b9530f22071b528d1
Nicolas Bernadine <nicolas.bernadine@gmail.com>
2013-02-26 23:28:21 +0100

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Albumslist::Application.config.secret_token = '668a2f85f4f69c4cf2260f32ebc14b4ef9e8442afb9c4199031b4b356e9a254303ca585e4464d8d9929ecc3638d80e676969c75d82085ef63d7965c3265438ff'
